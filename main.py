2a9801bfb30ebc4becce39c57ce3edd4e1c15207
Pratik Turakhiya <58838567+PratikTurakhiya@users.noreply.github.com>
2025-05-02 16:25:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+import os
 import pandas as pd
 from dash import Dash, dcc, html, Input, Output
 import dash_bootstrap_components as dbc
+import plotly.express as px
 import plotly.graph_objects as go
-import os
 
 # Load Excel data
 df = pd.read_excel("Juice_Sales_Data.xlsx")
@@ -133,6 +134,9 @@ def update_dashboard(start_date, end_date):
 
     return line_fig, pie_fig, f"₹ {total:,.0f}", f"₹ {cash:,.0f}", f"₹ {online:,.0f}", f"Week: {highest_day['Week'][0]} - {highest_day['Weekday'][0]}: ₹ {highest_day['Amount'][0]:,.0f}", f"₹ {avg_sales:,.0f}"
 
-# Gunicorn will look for this to start the app
 if __name__ == "__main__":
-    app.run_server(debug=False)
+    app.run_server(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 8050)),
+        debug=False
+    )
