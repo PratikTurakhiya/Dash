704c53146a4a9aaa143a7ca3aa7095561cc56297
Pratik Turakhiya <58838567+PratikTurakhiya@users.noreply.github.com>
2025-05-02 16:08:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -134,6 +134,8 @@ def update_dashboard(start_date, end_date):
 
     return line_fig, pie_fig, f"₹ {total:,.0f}", f"₹ {cash:,.0f}", f"₹ {online:,.0f}", f"Week: {highest_day['Week'][0]} - {highest_day['Weekday'][0]}: ₹ {highest_day['Amount'][0]:,.0f}", f"₹ {avg_sales:,.0f}"
 
+server = app.server 
+
 if __name__ == "__main__":
     app.run_server(
         host="0.0.0.0",
